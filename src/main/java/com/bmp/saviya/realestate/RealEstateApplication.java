4221ae26ec9236ffafc413df4d40ae4fbe1c3c5e
dahamv <dahamviharana@gmail.com>
2019-08-17 07:34:46 +0000

    Adding /greeing path mapping

--- src/main/java/com/bmp/saviya/realestate/RealEstateApplication.java
+++ src/main/java/com/bmp/saviya/realestate/RealEstateApplication.java
@@ -1,13 +1,52 @@
 package com.bmp.saviya.realestate;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class RealEstateApplication {
+
+    @RequestMapping("/")
+	String home() {
+		return "Welcome to the Front Page!";
+	}
+
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealEstateApplication.class, args);
 	}
 
+}
+
+class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+         this.id = id;
+         this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+         return content;
+    }
 }
