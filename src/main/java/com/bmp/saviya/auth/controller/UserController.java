e7751443a0f98031a59ea3af58e2e6976eb5aba9
dahamv <dahamviharana@gmail.com>
2019-09-20 08:50:52 +0000

    update

--- src/main/java/com/bmp/saviya/auth/controller/UserController.java
+++ src/main/java/com/bmp/saviya/auth/controller/UserController.java
@@ -25,10 +25,10 @@ public class UserController {
             user.setUsername(username.toString());
             user.setPassword(password.toString());
             userService.save(user);
-            return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+            return new ResponseEntity<String>("User registered successfully", HttpStatus.OK);
 
         } else {
-            return new ResponseEntity<>("username and password cannot be empty", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>("username and password cannot be empty", HttpStatus.BAD_REQUEST);
         }
     }
 }
